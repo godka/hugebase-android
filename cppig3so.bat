2febb597c23f46ab982d3de20bdc531d513a589b
cd <cd@b8c27e4c-60f7-f543-8d3b-5cd31ee1cda5>
2014-10-28 13:04:38 +0000

    改了什么忘了
    git-svn-id: https://svn.mythkast.net/svn/pig3android/trunk@28 b8c27e4c-60f7-f543-8d3b-5cd31ee1cda5

--- cppig3so.bat
+++ cppig3so.bat
@@ -1,4 +1,5 @@
-copy ..\src\bin\libkys*.so jni\kys_pig3\
+copy ..\develop\bin\libkys*.so jni\kys_pig3\
+copy ..\develop\bin\libkys*.so libs\armeabi\
 del *.apk
 move bin\SDLActivity.apk SDLActivity.apk
 dir /w
